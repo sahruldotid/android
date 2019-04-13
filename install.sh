e55991a07f0e921a19ccb865869878471131fc69
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2019-04-13 16:04:05 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "UPGRADING NODEJS & NPM"
+apt update
+apt install default-jre
+apt install default-jdk
+npm cache clean -f 
+npm install -g n
+npm i -g npm
+n stable
+ln -sf /usr/local/n/versions/node/10.15.3/bin/node /usr/bin/node
+
+
+
+
+echo "INSTALLING ANDROID"
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -O /tmp/sdk.zip
+unzip /tmp/sdk.zip -d /opt/android
+export ANDROID_HOME=/opt/android
+echo "export ANDROID_HOME=/opt/android" >> ~/.bashrc
+
+
+echo "SETTING PERMISSION LICENSE"
+export JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee'
+yes | /opt/android/tools/bin/sdkmanager --licenses
+
+
+
+echo "INSTALLING ANDROID NOUGAT"
+echo "check ./sdkmanager --list for more package"
+/opt/android/tools/bin/sdkmanager "platforms;android-25" "build-tools;25.0.1" "add-ons;addon-google_apis-google-24"
+
+
+
+echo "INSTALLING REACT NATIVE & EXPO.IO"
+npm install -g react-native-cli
+npm install -g expo-cli
+
+echo "DISCLAIMER: DONT FORGET TO CHANGE app.json when build android"
+echo "Enjoy your Tools :)"
